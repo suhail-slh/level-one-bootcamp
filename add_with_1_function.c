e40af3e1c27dfc6e73317b11921e81cfbce22526
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-05 00:53:42 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -3,7 +3,7 @@
 void main()
 {
 	int a,b;
-	printf(“Enter two numbers: ”);
-	scanf(“%d%d”,&a,&b);
-	printf(“Sum: %d\n”,a+b);
+	printf("Enter two numbers: ");
+	scanf("%d%d",&a,&b);
+	printf("Sum: %d\n",a+b);
 }
