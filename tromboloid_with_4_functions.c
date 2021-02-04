e3531a978f28272e317f0286c6c412c67269fd1e
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-05 01:32:35 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -3,16 +3,16 @@
 double h,d,b,vol;
 void get_params()
 {
-	printf(“Enter h,d,b values: ”);
-	scanf(“%lf%lf%lf”,&h,&d,&b);
+	printf("Enter h,d,b values: ");
+	scanf("%lf%lf%lf",&h,&d,&b);
 }
 void find_vol()
 {
-  vol = ((h*d*b)+(d/b))/3;
+	vol = ((h*d*b)+(d/b))/3;
 }
 void display_vol()
 {
-	printf(“Volume: %lf\n”,vol);
+	printf("Volume: %lf\n",vol);
 }
 int main()
 {
