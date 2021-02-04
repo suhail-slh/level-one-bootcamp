cb8d6fb82b432750e01adbe60a8c38dcc19b278b
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-04 17:16:30 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,1 +1,10 @@
 //Write the program to print "Hello World !"
+#include <stdio.h>
+#define MAX_SIZE 100
+void main()
+{
+	char name[MAX_SIZE];
+	printf(“Enter your name: ”);
+	scanf(“%s”,name);
+	printf(“Your name: %s\n”,name);
+}
