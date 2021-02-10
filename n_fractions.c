461529c93b307bf030e54c11982afae52dc1db4e
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-10 21:26:34 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,28 @@
 //WAP to find the sum of n fractions.
+#include <stdio.h>
+struct fraction{ double num,den; };
+void get_nums(struct fraction *f, int n)
+{
+	printf(“Enter the numerators, denominators of the fractions:  ”);
+	for(int i=0;i<n;i++)	scanf(“%lf%lf”,&f[i].num,&f[i].den);
+}
+double add_up(struct fraction *f, int n)
+{	
+  double sum = 0;
+  for(int i=0;i<n;i++)		sum += (f[i].num/f[i].den);
+  return sum;
+}
+void display_sum(double sum)
+{	printf(“Sum: %lf\n”,sum);	}
+int main()
+{
+	int n;	
+  double sum;
+	printf(“Enter the size of the array: ”);
+	scanf(“%d”,&n);
+	struct fraction f[n];
+	get_nums(f, n);
+	sum=add_up(f, n);
+	display_sum(sum);
+	return 0;
+}
