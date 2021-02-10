091877a9c561d25188e720d47e128afa73639a46
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-10 22:34:44 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,18 +1,30 @@
 //WAP to find the sum of two fractions.
 #include <stdio.h>
-struct fraction{ double num,den; }f1,f2;
+struct fraction{ int num,den; }f1,f2;
 double sum;
 void get_params()
 {
 	printf("Enter the numerator & denominator of the first fraction:	 ");
-	scanf("%lf%lf",&f1.num,&f1.den);
+	scanf("%d%d",&f1.num,&f1.den);
 	printf("Enter the numerator & denominator of the second fraction:	 ");
-	scanf("%lf%lf",&f2.num,&f2.den);
+	scanf("%d%d",&f2.num,&f2.den);
 }
 void find_sum()
-{	sum = (f1.num/f1.den) + (f2.num/f2.den);	}
+{	sum = ((double)f1.num/f1.den) + ((double)f2.num/f2.den);	}
+int get_gcd(int a, int b)
+{
+	if(a==0)	return b;
+	return get_gcd(b%a, a);
+}
 void display_sum()
-{	printf("Sum: %lf\n",sum);	}
+{	
+	int num, den, x;
+	sum *= 100;
+	x = get_gcd(sum, 100);
+	num = sum/x;
+	den = 100/x;
+    	printf("Sum: %d/%d\n",num,den);	
+}
 int main()
 {
 	get_params();
