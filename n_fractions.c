ca7a5f252e9d2c576f6512654c9a5132b4336a4b
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-10 21:27:05 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -3,23 +3,23 @@
 struct fraction{ double num,den; };
 void get_nums(struct fraction *f, int n)
 {
-	printf(“Enter the numerators, denominators of the fractions:  ”);
-	for(int i=0;i<n;i++)	scanf(“%lf%lf”,&f[i].num,&f[i].den);
+	printf("Enter the numerators, denominators of the fractions:  ");
+	for(int i=0;i<n;i++)	scanf("%lf%lf",&f[i].num,&f[i].den);
 }
 double add_up(struct fraction *f, int n)
 {	
-  double sum = 0;
-  for(int i=0;i<n;i++)		sum += (f[i].num/f[i].den);
-  return sum;
+	double sum = 0;
+	for(int i=0;i<n;i++)		sum += (f[i].num/f[i].den);
+	return sum;
 }
 void display_sum(double sum)
-{	printf(“Sum: %lf\n”,sum);	}
+{	printf("Sum: %lf\n",sum);	}
 int main()
 {
 	int n;	
-  double sum;
-	printf(“Enter the size of the array: ”);
-	scanf(“%d”,&n);
+	double sum;
+	printf("Enter the size of the array: ");
+	scanf("%d",&n);
 	struct fraction f[n];
 	get_nums(f, n);
 	sum=add_up(f, n);
