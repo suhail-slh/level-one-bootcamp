4d0884c058cde54ea0f067afe5876c7551eafb0a
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-19 21:58:32 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,25 +1,25 @@
 //WAP to find the distance between two point using 4 functions.
 #include <stdio.h>
 #include <math.h>
-double x_1,y_1,x_2,y_2,distance;
-void get_params()
+void get_coordinates(double *x_1, double *y_1, double *x_2, double *y_2)
 {
-	printf("Enter the coordinates x_1,y_1,x_2,y_2: ");
-	scanf("%lf%lf%lf%lf",&x_1,&y_1,&x_2,&y_2);
+    printf("Enter the coordinates x_1,y_1,x_2,y_2: ");
+    scanf("%lf%lf%lf%lf",x_1,y_1,x_2,y_2);
 }
-void find_dist()
+double find_dist(double x_1, double y_1, double x_2, double y_2)
 {
-	distance = sqrt( pow(x_2-x_1,2) + pow(y_2-y_1,2) );
+    double distance = sqrt( pow(x_2-x_1,2) + pow(y_2-y_1,2) );
+    return distance;
 }
-void display_dist()
+void display_dist(double x_1, double y_1, double x_2, double y_2)
 {
-	printf("Distance: %lf\n",distance);
+    double distance = find_dist(x_1, y_1, x_2, y_2);
+    printf("Distance: %lf\n",distance);
 }
 int main()
 {
-	get_params();
-	find_dist();
-	display_dist();
-
-	return 0;
+    double x_1,y_1,x_2,y_2;
+    get_coordinates(&x_1, &y_1, &x_2, &y_2);
+    display_dist(x_1, y_1, x_2, y_2);
+    return 0;
 }
