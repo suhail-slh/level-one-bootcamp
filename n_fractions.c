09955ba29ae4a559315b846694c5b4a69cbfa33f
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-19 22:50:28 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,41 +1,67 @@
 //WAP to find the sum of n fractions.
 #include <stdio.h>
+#define MAX_SIZE 1000
 struct fraction{ int num,den; };
-void get_nums(struct fraction *f, int n)
+void get_fracs(struct fraction f[], int *n)
 {
-	printf("Enter the numerators, denominators of the fractions:  ");
-	for(int i=0;i<n;i++)	scanf("%d%d",&f[i].num,&f[i].den);
+    printf("Enter the size of the array: ");
+    scanf("%d",n);
+    printf("Enter the numerators, denominators of the fractions:  ");
+    for(int i=0;i<(*n);i++)	
+        scanf("%d%d",&f[i].num,&f[i].den);
 }
-double add_up(struct fraction *f, int n)
-{	
-	double sum = 0;
-	for(int i=0;i<n;i++)		sum += ((double)f[i].num/f[i].den);
-	return sum;
+int get_lcm(int a, int b)
+{
+    int c, mul;
+    c = a>b ? a : b;
+    mul = c;
+    while(mul%a || mul%b)
+        mul+=c;
+    return mul;
 }
 int get_gcd(int a, int b)
 {
-	if(a==0)    return b;
-	return get_gcd(b%a, a);
+    int temp;
+    while(a!=0)
+    {
+        temp=a;
+        a=b%a;
+        b=temp;
+    }
+    return b;
 }
-void display_sum(double sum)
+struct fraction find_sum(struct fraction f[], int n)
 {	
-	int num, den, x, y;
-	y = 100;
-	sum *= y;
-	x = get_gcd(sum, y);
-	num = sum/x;
-	den = y/x;
-	printf("Sum: %d/%d\n",num,den);	
+    int i, gcd;
+    struct fraction sum;
+    if(n)
+        sum.den = f[0].den;
+    for(i=1;i<n;i++)
+        sum.den = get_lcm(sum.den, f[i].den);
+    sum.num = 0;
+    for(i=0;i<n;i++)
+        sum.num += (f[i].num * (sum.den/f[i].den));
+    gcd = get_gcd(sum.num, sum.den);
+    sum.num /= gcd;
+    sum.den /= gcd;
+    return sum;
+}
+void display_sum(struct fraction *f, int n)
+{	
+    struct fraction sum = find_sum(f, n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d/%d ",f[i].num,f[i].den);
+        if(i<n-1)
+           printf(" + ");
+    }
+    printf(" = %d/%d\n", sum.num, sum.den);	
 }
 int main()
 {
-	int n;	
-	double sum;
-	printf("Enter the size of the array: ");
-	scanf("%d",&n);
-	struct fraction f[n];
-	get_nums(f, n);
-	sum=add_up(f, n);
-	display_sum(sum);
-	return 0;
+    int n;
+    struct fraction f[MAX_SIZE];
+    get_fracs(f, &n);
+    display_sum(f, n);
+    return 0;
 }
