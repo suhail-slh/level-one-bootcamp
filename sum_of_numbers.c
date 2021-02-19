e5c727fb9837fe806709cff70325d8f9e9abe4b5
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2021-02-19 22:49:43 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,26 +1,36 @@
 //Write a program to find the sum of n different numbers using 4 functions
 #include <stdio.h>
-void get_nums(int *a, int n)
+#define MAX_SIZE 1000
+void get_nums(int a[], int *n)
 {
-	printf("Enter the elements of the array:  ");
-	for(int i=0;i<n;i++)	scanf("%d",&a[i]);
+    printf("Enter the size of the array: ");
+    scanf("%d",n);
+    printf("Enter the elements of the array:  ");
+    for(int i=0;i<(*n);i++)	
+        scanf("%d",&a[i]);
 }
-int add_up(int *a, int n)
+int add_up(int a[], int n)
 {	
-	int sum = 0;
-	for(int i=0;i<n;i++)		sum += a[i];
-	return sum;
+    int sum = 0;
+    for(int i=0;i<n;i++)		
+        sum += a[i];
+    return sum;
 }
-void display_sum(int sum)
-{	printf("Sum: %d\n",sum);	}
+void display_sum(int a[], int n)
+{	
+    int sum = add_up(a, n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+        if(i<n-1)
+            printf("+ ");
+    }
+    printf("= %d\n",sum);	
+}
 int main()
 {
-	int n, sum;
-	printf("Enter the size of the array: ");
-	scanf("%d",&n);
-	int a[n];
-	get_nums(a, n);
-	sum=add_up(a, n);
-	display_sum(sum);
-	return 0;
+    int a[MAX_SIZE], n;
+    get_nums(a, &n);
+    display_sum(a, n);
+    return 0;
 }
